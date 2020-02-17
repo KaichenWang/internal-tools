5fb193f7442fd7cc40b6e464b87870fe2c50f80c
Kai <kai.wang@shopify.com>
2020-02-17 13:20:33 -0500

    Update indents. Ensure 2 column layout

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,8 @@ import {
     Page,
     Card,
     Button,
-  } from '@shopify/polaris';
-import banner from '../assets/banner.png'; 
+} from '@shopify/polaris';
+import banner from '../assets/banner.png';
 import constants from '../constants'
 
 import {
@@ -19,25 +19,28 @@ function Home() {
     return (
         <Page title="Utilities">
             <Layout>
-            <Layout.Section>
-            <img alt="Utilities" src={banner} style={{width: "100%", marginBottom: "1em"}}></img>
-            </Layout.Section>
+                <Layout.Section>
+                    <img alt="Utilities" src={banner} style={{ width: "100%", marginBottom: "1em" }}></img>
+                </Layout.Section>
             </Layout>
             <Layout>
                 {Object.keys(utils).map(key => {
-                return (
-                    <Layout.Section oneThird key={key}>
-                        <Card title={utils[key].title}>
-                            <Card.Section>{utils[key].description}</Card.Section>
-                            <Card.Section>
-                            <Link to={utils[key].path}>
-                                <Button>Try it</Button>
-                            </Link>
-                            </Card.Section>
-                        </Card> 
-                    </Layout.Section>
-                );
+                    return (
+                        <Layout.Section oneHalf key={key}>
+                            <Card title={utils[key].title}>
+                                <Card.Section>{utils[key].description}</Card.Section>
+                                <Card.Section>
+                                    <Link to={utils[key].path}>
+                                        <Button>Try it</Button>
+                                    </Link>
+                                </Card.Section>
+                            </Card>
+                        </Layout.Section>
+                    );
                 })}
+                {Object.keys(utils).length % 2 !== 0 &&
+                    <Layout.Section oneHalf></Layout.Section>
+                }
             </Layout>
         </Page>
     );
